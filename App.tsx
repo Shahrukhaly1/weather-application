a1ec19f475590a40385c9274757543e20bb8d5c7
Shahrukhaly1 <154752841+Shahrukhaly1@users.noreply.github.com>
2024-04-06 16:01:14 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { Text, View } from 'react-native';
+import Home from './src/Home';
+import Details from './src/Details';
+
+// Define your Home component
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Details' component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
